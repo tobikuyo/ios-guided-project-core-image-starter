3b6f7778ef80e6a2099dbb1c34c88ae85e068680
Tobi Tselios <tobi.tselios@gmail.com>
2020-05-04 14:44:43 +0100

    Added Properties and a filterImage Method, with the Steps Required Commented

--- PhotoFilter/PhotoFilterViewController.swift
+++ PhotoFilter/PhotoFilterViewController.swift
@@ -1,18 +1,42 @@
 import UIKit
 import CoreImage
+import CoreImage.CIFilterBuiltins
 import Photos
 
 class PhotoFilterViewController: UIViewController {
+
+    // MARK: - Outlets
 
 	@IBOutlet weak var brightnessSlider: UISlider!
 	@IBOutlet weak var contrastSlider: UISlider!
 	@IBOutlet weak var saturationSlider: UISlider!
 	@IBOutlet weak var imageView: UIImageView!
+
+    // MARK: - Properties
+
+    private var originalImage: UIImage?
+    private var context = CIContext(options: nil)
 	
 	override func viewDidLoad() {
 		super.viewDidLoad()
 
+        let filter = CIFilter.colorControls()
+        print(filter)
+        print(filter.attributes)
 	}
+
+    func filterImage(_ image: UIImage) -> UIImage? {
+
+        // UIIMAGE -> CGIMAGE (Core Graphics) -> CIImage (Core Image)
+
+        // Filter
+
+        // Render the Image
+
+        // CIImage -> CGImage -> UIImage
+
+        return nil
+    }
 	
 	// MARK: Actions
 	
